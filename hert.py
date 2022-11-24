a8fb0e9c3c801411966923455a232595aa43741e
Bahabit254 <108923805+Bahabit254@users.noreply.github.com>
2022-11-24 11:19:44 +0300

    Update hert.py

--- hert.py
+++ hert.py
@@ -79,14 +79,14 @@ def main():
         st.markdown("""<style>div[data-testid="metric-container"] {background-color: rgba(28, 131, 225, 0.1);border: 1px solid rgba(28, 131, 225, 0.1);padding: 5% 5% 5% 10%;border-radius: 5px;color: rgb(30, 103, 119);overflow-wrap: break-word;} /* breakline for metric text         */div[data-testid="metric-container"] > label[data-testid="stMetricLabel"] > div {overflow-wrap: break-word;white-space: break-spaces;color: green;font-size: 20px;} </style>""", unsafe_allow_html=True)
 
         with precision:
-            st.metric(label="Precision Score", value="86%")
+            st.metric(label="Precision Score", value="84%")
         with recall:
-            st.metric(label="Recall Score", value="86%")
+            st.metric(label="Recall Score", value="84%")
     
         with f1:
-            st.metric(label="F1 Score", value="86%")
+            st.metric(label="F1 Score", value="84%")
         with acc:
-            st.metric(label="Accuracy Score", value="86%")
+            st.metric(label="Accuracy Score", value="84%")
 
 
 
